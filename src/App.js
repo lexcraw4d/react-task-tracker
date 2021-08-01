9cf57886965e0242a0c5fac04ff4d4beae33e94c
Lex Crawford <lexcraw4d@gmail.com>
2021-08-01 19:07:45 -0400

    added tasks into seperate components and adjusted background color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from "react"
 
-function App() {
+const App = () => {
   
+  const [ tasks, setTasks ] = useState (
+    [
+        {
+            id:1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5h at 2:30 pm',
+            reminder: true,
+        },
+        {
+            id:2,
+            text: 'Meeting at School',
+            day: 'Feb 6h at 1:30 pm',
+            reminder: true,
+        },
+        {
+            id:3,
+            text: 'Food Shopping',
+            day: 'Feb 5h at 2:30 pm',
+            reminder: false,
+        }
+    ]
+)
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={ tasks }/>
       
     </div>
   );
